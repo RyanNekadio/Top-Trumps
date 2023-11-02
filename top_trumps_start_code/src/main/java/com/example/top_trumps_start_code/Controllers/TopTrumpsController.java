3505e3d37948dc576f6706886711da917e34efe9
RyanNekadio <ryanekadio12@hotmail.co.uk>
2023-11-02 09:53:54 +0000

    Uploading refactored code solutions

--- top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/Controllers/TopTrumpsController.java
+++ top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/Controllers/TopTrumpsController.java
@@ -2,32 +2,24 @@ package com.example.top_trumps_start_code.Controllers;
 
 import com.example.top_trumps_start_code.Services.TopTrumpsService;
 import com.example.top_trumps_start_code.models.Card;
-import com.example.top_trumps_start_code.models.Reply;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping(value = "/cardgames")
+@RequestMapping(value = "/toptrumps")
 public class TopTrumpsController {
 
     @Autowired
     TopTrumpsService topTrumpsService;
 
     @PostMapping
-    public ResponseEntity<Reply> newGame(@RequestBody ArrayList<Card> cards){
-        Reply reply = topTrumpsService.startNewGame();
-        return new ResponseEntity<Reply>(reply, HttpStatus.CREATED);
-    }
-
-    @PatchMapping
-    public ResponseEntity<Reply> handleGuess(@RequestBody ArrayList<Card> cards){
-        Reply reply = topTrumpsService.checkWinner(cards);
-        return new ResponseEntity<>(reply, HttpStatus.OK);
+    public ResponseEntity<String> newGame(@RequestBody ArrayList<Card> cards){
+        String winner = topTrumpsService.checkWinner(cards);
+        return new ResponseEntity<>(winner, HttpStatus.OK);
     }
 
 }
--- top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/Services/TopTrumpsService.java
+++ top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/Services/TopTrumpsService.java
@@ -1,9 +1,6 @@
 package com.example.top_trumps_start_code.Services;
 
 import com.example.top_trumps_start_code.models.Card;
-import com.example.top_trumps_start_code.models.Rank;
-import com.example.top_trumps_start_code.models.Reply;
-import com.example.top_trumps_start_code.models.Suit;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,69 +8,15 @@ import java.util.ArrayList;
 @Service
 public class TopTrumpsService {
 
-    private Card card1;
-    private Card card2;
-    private ArrayList<Card> drawnCards;
-
-    public TopTrumpsService(){
-    }
-
-    public Reply startNewGame() {
-//        Extension task 1: code to test draw
-//        this.card1 = new Card(Rank.JACK, Suit.HEARTS);
-//        this.card2 = new Card(Rank.JACK, Suit.CLUBS);
-        this.card1 = new Card(Rank.SEVEN, Suit.DIAMONDS);
-        this.card2 = new Card(Rank.TWO, Suit.SPADES);
-        this.drawnCards = new ArrayList<>();
-        drawnCards.add(card1);
-        drawnCards.add(card2);
-        return new Reply(
-                "Started new game"
-        );
-    }
-//    MVP task: Check winning card
-    public Reply checkWinner(ArrayList<Card> cards){
-        cards.add(this.card1);
-        cards.add(this.card2);
-        Reply reply;
-
-        if (card1.getCardValue() > card2.getCardValue()){
-            reply = new Reply(card1.getRank() + " of " + card1.getSuit() + " wins!");
-            reply.setWinner(true);
-            return reply;
-        } else if (card1.getCardValue() < card2.getCardValue()) {
-            reply = new Reply(card2.getRank() + " of" + card2.getSuit() + " wins!");
-            reply.setWinner(true);
-            return reply;
-//        Extension task: Add message in the event of a draw
+    public String checkWinner(ArrayList<Card> cards){
+        if (cards.get(0).getCardValue() > cards.get(1).getCardValue()){
+            return cards.get(0).getRank() + " of " + cards.get(0).getSuit() + " wins!";
+        } else if (cards.get(0).getCardValue() < cards.get(1).getCardValue()){
+            return cards.get(1).getRank() + " of " + cards.get(1).getSuit() + " wins!";
+//            Extension task 1: code in the event of a draw
         } else {
-            reply = new Reply("Draw! card values are the same!");
-            return reply;
+            return "Draw!";
         }
-    }
-
-    public Card getCard1() {
-        return card1;
-    }
-
-    public void setCard1(Card card1) {
-        this.card1 = card1;
-    }
-
-    public Card getCard2() {
-        return card2;
-    }
-
-    public void setCard2(Card card2) {
-        this.card2 = card2;
-    }
-
-    public ArrayList<Card> getDrawnCards() {
-        return drawnCards;
-    }
-
-    public void setDrawnCards(ArrayList<Card> drawnCards) {
-        this.drawnCards = drawnCards;
     }
 
 }
